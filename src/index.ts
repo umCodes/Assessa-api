e8b79aa375ea484fda292118bcb748d36b86c104
umCodes <umohammed.official@gmail.com>
2025-12-28 14:54:28 +0300

    Comment out Tesseract.js worker code
    Comment out the Tesseract.js worker initialization and recognition code.

--- src/index.ts
+++ src/index.ts
@@ -24,17 +24,17 @@ import { ORIGIN, PORT } from './constants/env';
 import { createWorker } from 'tesseract.js';
 import path from 'path';
 
-(async () => {
-    const worker = await createWorker(); // specify language (English)
-    await worker.loadLanguage('eng');
-    await worker.initialize('eng');
+// (async () => {
+//     const worker = await createWorker(); // specify language (English)
+//     await worker.loadLanguage('eng');
+//     await worker.initialize('eng');
 
-    const imagePath = path.join( __dirname, "../", "/uploads", 'images.png');
-    const { data: { text } } = await worker.recognize(imagePath);
-    console.log(text);
+//     const imagePath = path.join( __dirname, "../", "/uploads", 'images.png');
+//     const { data: { text } } = await worker.recognize(imagePath);
+//     console.log(text);
 
-    await worker.terminate()
-});
+//     await worker.terminate()
+// });
 
 
 
