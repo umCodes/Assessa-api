86f3c2d6dbe411887e79f5387a8d6ae006a25f55
umCodes <umohammed.official@gmail.com>
2026-03-15 04:38:31 +0300

    Update refreshServer.ts

--- src/cron/refreshServer.ts
+++ src/cron/refreshServer.ts
@@ -4,12 +4,12 @@ const SERVER_URL = process.env.SERVER_URL!;
 
 export function startRefreshCron() {
   // Every 10 minutes
-  cron.schedule("*/10 * * * *", async () => {
-    try {
-      const res = await fetch(SERVER_URL);
-      console.log(`[CRON] Server pinged: ${res.status}`);
-    } catch (err) {
-      console.error("[CRON] Ping failed:", err);
-    }
-  });
+  //cron.schedule("*/10 * * * *", async () => {
+    //try {
+      //const res = await fetch(SERVER_URL);
+      //console.log(`[CRON] Server pinged: ${res.status}`);
+    //} catch (err) {
+      //console.error("[CRON] Ping failed:", err);
+    //}
+  //});
 }
